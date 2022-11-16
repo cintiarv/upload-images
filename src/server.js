df97d5ce2ea2629a81aa83ed400204faf338f897
Cintia Ruiz <cintiaruizv@gmail.com>
2022-11-15 22:59:04 -0600

    fewjfew

--- src/server.js
+++ src/server.js
@@ -1,9 +1,11 @@
 import express from 'express'
+import productsRouter from '../src/routers/products.router.js'
 
 const server = express()
 
 server.use(express.json())
 
 
+server.use('/products', productsRouter)
 
 export { server }
